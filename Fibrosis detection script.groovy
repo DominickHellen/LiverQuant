0d626b0d11fd92b0e3a335bb349bece52e941473
D Hellen <88243822+DominickHellen@users.noreply.github.com>
2023-01-31 10:51:17 -0500

    Update Fibrosis detection script.groovy

--- Fibrosis detection script.groovy
+++ Fibrosis detection script.groovy
@@ -5,25 +5,31 @@
 //It also clears accumulated RAM for batch projects//
 Thread.sleep(100)
 javafx.application.Platform.runLater {
-    getCurrentViewer().getImageRegionStore().cache.clear()
-    System.gc()
+getCurrentViewer().getImageRegionStore().cache.clear()
+System.gc()
 }
 Thread.sleep(100)
 
 //Allows Qupath to make decisions for positive staining based on staining that is residual to DAB and hematoxylin//
 setImageType('BRIGHTFIELD_OTHER');
 
-//Selelcts annotations on each slide//
+// This should be deleted if you want to annotate by hand
+clearSelectedObjects();
+
+// This should be deleted if you want to annotate by hand
+//Increase or decrease the 'threshold' number below to improve accuracy of automated tissue annotation
+runPlugin('qupath.imagej.detect.tissue.SimpleTissueDetection2', '{"threshold":178,"requestedPixelSizeMicrons":200.0,"minAreaMicrons":10000.0,"maxHoleAreaMicrons":1000000.0,"darkBackground":false,"smoothImage":false,"medianCleanup":true,"dilateBoundaries":false,"smoothCoordinates":true,"excludeOnBoundary":true,"singleAnnotation":true}')
+
+//Selects annotations on each slide//
 selectAnnotations();
 
 //Quantifies positive pixel amount (Fibrotic septa) for entire all annotations//
-addPixelClassifierMeasurements("Fibrosis", "Fibrosis")
+addPixelClassifierMeasurements("LiverQuantF", "LiverQuantF")
 
 //Clears RAM and thread after each slide//
 Thread.sleep(100)
 javafx.application.Platform.runLater {
-    getCurrentViewer().getImageRegionStore().cache.clear()
-    System.gc()
+getCurrentViewer().getImageRegionStore().cache.clear()
+System.gc()
 }
 Thread.sleep(100)
-
